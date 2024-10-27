eb7cf6d8345f67b0ecf23616d553ab497535e432
dszhusb <71653499+dszhusb@users.noreply.github.com>
2024-10-27 15:53:20 -0400

    Folder Icon

--- src/lib/components/shell/Folder.svelte
+++ src/lib/components/shell/Folder.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
+    import folderSvg from "$lib/assets/icons/folder.svg";
     export let name;
     export let color;
     let isOpen = true;
 </script>
 
 <div class="container" style:background-color={`${color}10`}>
-    <button on:click={() => (isOpen = !isOpen)} style:background-color={`${color}40`}>
+    <button
+        on:click={() => (isOpen = !isOpen)}
+        style:background-color={`${color}40`}
+    >
         <p>{name}</p>
-        <p>{isOpen ? "v" : ">"}</p>
+        <img
+            src={folderSvg}
+            alt="folder"
+            class={`max-w-5 ${!isOpen && "rotated"}`}
+        />
     </button>
     {#if isOpen}
         <div class="content">
@@ -25,7 +33,11 @@
         @apply p-4 border-stone-800 border-t-[1px];
     }
 
+    .rotated {
+        @apply rotate-180;
+    }
+
     button {
-        @apply w-full uppercase text-stone-800 py-2 px-3 flex justify-between;
+        @apply w-full uppercase text-stone-800 py-2 px-3 flex justify-between items-center;
     }
 </style>
